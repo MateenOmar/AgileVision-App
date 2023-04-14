9cb150f0dffeb30a0854a63d123e1e6eba56962a
Kazi Shafin Shafayet <51491019+Ridr12@users.noreply.github.com>
2023-04-13 23:59:17 -0400

    added issue functions

--- kanbanwindow.cpp
+++ kanbanwindow.cpp
@@ -29,6 +29,10 @@ Kanbanwindow::Kanbanwindow(QWidget *parent, QString userEmail, QString projectNa
 
     select_sql();
 
+    select_sprint();
+
+    select_Issues();
+
     socket = new QTcpSocket(this);
 
     connect(socket, &QTcpSocket::readyRead, [&](){
@@ -161,7 +165,15 @@ QListWidgetItem Kanbanwindow::update_item(QListWidgetItem *item, QString column)
     TextEdit_1->setGeometry(150,70,150,100);
     TextEdit_1->setParent(dialog);
     if (column != "new"){
-        //select
+        db2.open();
+        QSqlQuery qry_select(db2);
+        qry_select.prepare("SELECT * FROM Tasks WHERE tname=?;");
+        qry_select.addBindValue(item->text());
+        qry_select.exec();
+        while(qry_select.next()){
+            TextEdit_1->setText(qry_select.value(1).toString());
+        }
+        db2.close();
     }
     TextEdit_1->show();
 
@@ -175,7 +187,6 @@ QListWidgetItem Kanbanwindow::update_item(QListWidgetItem *item, QString column)
     if (dialog->exec() == QDialog::Accepted){
         if (column == "new"){
             insert_sql(LineEdit_1->text(), TextEdit_1->toPlainText(), "BacklogList", "831 Project");
-            //select_sql();
         } else {
             update_sql(item->text(), TextEdit_1->toPlainText(), column, "831 Project", LineEdit_1->text());
         }
@@ -192,8 +203,6 @@ void Kanbanwindow::on_DoneList_itemDoubleClicked(QListWidgetItem *item)
 {
     item->setText(update_item(item, "DoneList").text());
 }
-
-
 
 void Kanbanwindow::on_InReviewList_itemDoubleClicked(QListWidgetItem *item)
 {
@@ -261,5 +270,221 @@ void Kanbanwindow::on_refreshButton_clicked()
     ui->InReviewList->clear();
     ui->DoneList->clear();
     select_sql();
+}
+
+
+void Kanbanwindow::on_sprint1Button_clicked()
+{
+    QListWidgetItem *item = new QListWidgetItem();
+    item->setText("");
+    item->setText(update_item(item, "new").text());
+    insert_sprint(item->text(), 1, "831 project");
+    ui->sprint1List->addItem(item);
+    ui->BacklogList->addItem(item);
+}
+
+
+void Kanbanwindow::on_sprint2Button_clicked()
+{
+    QListWidgetItem *item = new QListWidgetItem();
+    item->setText("");
+    item->setText(update_item(item, "new").text());
+    insert_sprint(item->text(), 2, "831 project");
+    ui->sprint2List->addItem(item);
+    ui->BacklogList->addItem(item);
+}
+
+
+void Kanbanwindow::on_sprint3Button_clicked()
+{
+    QListWidgetItem *item = new QListWidgetItem();
+    item->setText("");
+    item->setText(update_item(item, "new").text());
+    insert_sprint(item->text(), 3, "831 project");
+    ui->sprint3List->addItem(item);
+    ui->BacklogList->addItem(item);
+}
+
+void Kanbanwindow::insert_sprint(QString tname, int snum, QString pname){
+    db2.open();
+    QSqlDatabase::database().transaction();
+    QSqlQuery qry_insert(db2);
+    qry_insert.prepare("INSERT INTO Sprints (tname,snumber,pname) VALUES (?,?,?)");
+    qry_insert.addBindValue(tname);
+    qry_insert.addBindValue(snum);
+    qry_insert.addBindValue(pname);
+    if (!qry_insert.exec()){
+        qDebug() << qry_insert.lastError();
+    }
+    QSqlDatabase::database().commit();
+    db2.close();
+}
+
+void Kanbanwindow::select_sprint(){
+    db2.open();
+    QSqlQuery qry_select(db2);
+    if (qry_select.exec("SELECT * FROM Sprints;")){
+        while(qry_select.next()){
+            if (qry_select.value(1) == 1){
+                ui->sprint1List->addItem(qry_select.value(0).toString());
+            } else if(qry_select.value(1) == 2){
+                ui->sprint2List->addItem(qry_select.value(0).toString());
+            } else if(qry_select.value(1) == 3){
+                ui->sprint3List->addItem(qry_select.value(0).toString());
+            }
+        }
+
+    }
+    db2.close();
+}
+
+
+
+void Kanbanwindow::on_commentButton_clicked()
+{
+    QString comment = ui->issueLine->text();
+    QString old_comment;
+    db2.open();
+    QSqlQuery qry_select(db2);
+    qry_select.prepare("SELECT * FROM Issues WHERE tname=?;");
+    qry_select.addBindValue(ui->issueLabel->text());
+    if (qry_select.exec()){
+        while (qry_select.next()){
+            old_comment = qry_select.value(1).toString();
+        }
+    }
+    db2.close();
+    QString new_comment;
+    if (old_comment == ""){
+        new_comment = this->userFullName + ": " + comment;
+    }else {
+        new_comment = old_comment + "\n\n" + this->userFullName + ": " + comment;
+    }
+    db2.open();
+    QSqlDatabase::database().transaction();
+    QSqlQuery qry_update(db2);
+    qry_update.prepare("UPDATE Issues SET icomment=? WHERE tname=?");
+    qry_update.addBindValue(new_comment);
+    qry_update.addBindValue(ui->issueLabel->text());
+    if (!qry_update.exec()){
+        qDebug() << qry_update.lastError();
+    }
+    QSqlDatabase::database().commit();
+    db2.close();
+    ui->issueComment->setText(new_comment);
+    ui->issueLine->clear();
+
+}
+
+
+void Kanbanwindow::on_newIssueList_itemDoubleClicked(QListWidgetItem *item)
+{
+    ui->issueLabel->setText(item->text());
+    db2.open();
+    QSqlQuery qry_select(db2);
+    qry_select.prepare("SELECT * FROM Issues WHERE tname=?");
+    qry_select.addBindValue(item->text());
+    if (qry_select.exec()){
+        while (qry_select.next()){
+            ui->issueComment->setText(qry_select.value(1).toString());
+        }
+    }
+    db2.close();
+}
+
+
+void Kanbanwindow::on_completedIssueList_itemDoubleClicked(QListWidgetItem *item)
+{
+    ui->issueLabel->setText(item->text());
+    db2.open();
+    QSqlQuery qry_select(db2);
+    qry_select.prepare("SELECT * FROM Issues WHERE tname=?");
+    qry_select.addBindValue(item->text());
+    if (qry_select.exec()){
+        while (qry_select.next()){
+            ui->issueComment->setText(qry_select.value(1).toString());
+        }
+    }
+    db2.close();
+}
+
+
+void Kanbanwindow::on_addIssue_clicked()
+{
+    QListWidgetItem *item = new QListWidgetItem();
+    item->setText("");
+    item->setText(update_item(item, "new").text());
+    insert_Issues(item->text(), "", "831 Project", "no");
+    ui->newIssueList->addItem(item);
+    ui->BacklogList->addItem(item);
+}
+
+void Kanbanwindow::select_Issues(){
+    db2.open();
+    QSqlQuery qry_select(db2);
+    if (qry_select.exec("SELECT * FROM Issues;")){
+        while(qry_select.next()){
+            if (qry_select.value(3) == "yes"){
+                ui->completedIssueList->addItem(qry_select.value(0).toString());
+            } else if(qry_select.value(3) == "no"){
+                ui->newIssueList->addItem(qry_select.value(0).toString());
+            }
+        }
+
+    }
+    db2.close();
+}
+
+void Kanbanwindow::insert_Issues(QString tname, QString icomment, QString pname, QString icomplete){
+    db2.open();
+    QSqlDatabase::database().transaction();
+    QSqlQuery qry_insert(db2);
+    qry_insert.prepare("INSERT INTO Issues (tname,icomment,pname,icomplete) VALUES (?,?,?,?)");
+    qry_insert.addBindValue(tname);
+    qry_insert.addBindValue(icomment);
+    qry_insert.addBindValue(pname);
+    qry_insert.addBindValue(icomplete);
+    if (!qry_insert.exec()){
+        qDebug() << qry_insert.lastError();
+    }
+    QSqlDatabase::database().commit();
+    db2.close();
+}
+
+void Kanbanwindow::save_Issues(){
+    for(int i = 0; i < ui->newIssueList->count(); i++){
+        update_save_Issues(ui->newIssueList->item(i)->text(), "no");
+    }
+    for(int i = 0; i < ui->completedIssueList->count(); i++){
+        update_save_Issues(ui->completedIssueList->item(i)->text(), "yes");
+    }
+}
+
+void Kanbanwindow::update_save_Issues(QString tname, QString icomplete){
+    db2.open();
+    QSqlDatabase::database().transaction();
+    QSqlQuery qry_update(db2);
+    qry_update.prepare("UPDATE Issues SET icomplete=? WHERE tname=?");
+    qry_update.addBindValue(icomplete);
+    qry_update.addBindValue(tname);
+    if (!qry_update.exec()){
+        qDebug() << qry_update.lastError();
+    }
+    QSqlDatabase::database().commit();
+    db2.close();
+}
+
+void Kanbanwindow::on_saveIssuesButton_clicked()
+{
+    save_Issues();
+    QMessageBox::information(this, "Saved Changes", "All changes made in the software are now saved.");
+}
+
+
+void Kanbanwindow::on_refreshIssues_clicked()
+{
+    ui->newIssueList->clear();
+    ui->completedIssueList->clear();
+    select_Issues();
 }
 
